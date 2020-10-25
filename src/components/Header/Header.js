cea41713f805d48d78769c3dd6275d8daef47ff7
Serhii Karavashkin <ksawl@narod.ru>
2020-10-25 16:48:50 +0200

    ref: bootstrap header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,60 @@
 import './Header.scss';
 
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
 import React from 'react';
+import { connect } from 'react-redux';
+import { showModalCreate } from '../../redux/actions';
 
-const Header = () => {
+const Header = ({ showModalCreate }) => {
+    const handleShowModal = (event) => {
+        event.preventDefault();
+
+        showModalCreate(true);
+    };
+
     return (
-        <header className="header">
-            <div className="header__wrap container">
-                <a href="/" className="header__logo">
+        <Navbar
+            bg="dark"
+            variant="dark"
+            expand="lg"
+            fixed="top"
+            collapseOnSelect
+        >
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src="/logo.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
                     CRUD
-                </a>
+                </Navbar.Brand>
 
-                <div className="header__buttons">
-                    <button className="btn btn-danger btn-add">ADD</button>
-                </div>
-            </div>
-        </header>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link
+                            href="#add"
+                            className="btn btn-danger btn-add"
+                            onClick={handleShowModal}
+                        >
+                            Add Item
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
-export default Header;
+const mapDispatchToProps = {
+    showModalCreate,
+};
+
+export default connect(null, mapDispatchToProps)(Header);
