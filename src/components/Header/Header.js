4626669eccb9fd8012e2c5715bae5242e2fccee2
Serhii Karavashkin <ksawl@narod.ru>
2021-01-02 16:33:02 +0200

    check

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -34,7 +34,7 @@ const Header = ({ showModalCreate, fetchRecipes }) => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#home">Home1</Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link
